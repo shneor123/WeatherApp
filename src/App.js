505349d7f5cb1debe3d839e14f6c22aaf0f60e6d
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2022-12-25 15:11:45 +0200

    fix home style end add pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom"
 
 import { HomePage } from "./pages/home-page";
+import { Search } from "./pages/Search";
+import { Favorites } from "./pages/Favorites";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route element={<HomePage />} path={'/'} />
+          <Route element={<Favorites />} path={'/favorites'} />
+          <Route element={<Search />} path={'/search'} />
         </Routes>
       </Router>
       {/* <AppHeader /> */}
